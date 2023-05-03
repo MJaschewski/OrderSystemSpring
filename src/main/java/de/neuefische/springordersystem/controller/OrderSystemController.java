60e6d37f5439b601631e5d676ba60c295e3eff25
mjaschew <dermijasch@gmx.de>
2023-05-03 11:26:54 +0200

    Commit updated controller

--- src/main/java/de/neuefische/springordersystem/controller/OrderSystemController.java
+++ src/main/java/de/neuefische/springordersystem/controller/OrderSystemController.java
@@ -1,10 +1,43 @@
 package de.neuefische.springordersystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.neuefische.springordersystem.model.Order;
+import de.neuefische.springordersystem.model.Product;
+import de.neuefische.springordersystem.service.ShopService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class OrderSystemController {
+@RequiredArgsConstructor
 
+public class OrderSystemController {
+    private final ShopService shopService;
+
+    @GetMapping("products")
+    public List<Product> getProducts(){
+        return shopService.listProducts();
+    }
+
+    @GetMapping("products/{id}")
+    public Product getProduct(@PathVariable int id){
+        return shopService.getProduct(id);
+    }
+
+    @GetMapping("orders")
+    public List<Order> getOrders(){
+        return shopService.listOrders();
+    }
+
+    @GetMapping("orders/{id}")
+    public Order getOrder(@PathVariable int id){
+        return shopService.getOrder(id);
+    }
+
+    @PostMapping("orders/{id}")
+    public Order postOrder(@PathVariable int id, @RequestBody List<Integer> orderList){
+        shopService.addOrder(id, orderList);
+        return shopService.getOrder(id);
+    }
 }
